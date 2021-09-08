6f691fbcab77ad7d259b3c5dd5a3447f1caa9ccb
oris-96 <iabdulkareem94@gmail.com>
2021-09-07 20:47:48 -0400

    new media

--- meme-me/src/App.js
+++ meme-me/src/App.js
@@ -1,12 +1,12 @@
 // bring in React from React
-import React from 'react'
+import React from 'react';
 
 // define our Welcome functional component
 function Welcome() {
-// what should the component return
+  // what should the component return
   return (
-  // Make sure to return some UI
-    <h1>Welcome to Meme-ification!</h1>
+    // Make sure to return some UI
+    <h1>Welcome to Meme-ification/!</h1>
   );
 }
 
