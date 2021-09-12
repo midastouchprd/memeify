ee9b4c860c974645107d6764d38c6127cc138c86
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-09-12 15:01:31 -0700

    make user folder front end

--- server/models/User.js
+++ server/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    photo: { type: string },
     favoriteMeme: {
       data: Buffer,
       contentType: String,
