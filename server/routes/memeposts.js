c949fca540b5dd7ba9b7bef1e9cd48ae8a3661e2
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-09-13 15:24:35 -0700

    create uploads folder in public

--- server/routes/memeposts.js
+++ server/routes/memeposts.js
@@ -3,4 +3,10 @@ const router = require('express').Router();
 const { memePost } = require('../controllers');
 const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./src/public/uploads/")
+  }
+})
+
 module.exports = router;
