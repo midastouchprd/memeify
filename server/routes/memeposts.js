765d70fff3ad91c5569f1b3499a672c33c600020
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-09-13 15:20:42 -0700

    change user route and memepost route

--- server/routes/memeposts.js
+++ server/routes/memeposts.js
@@ -1,5 +1,6 @@
 // Users Router
 const router = require('express').Router();
 const { memePost } = require('../controllers');
+const multer = require('multer');
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,49 +1,10 @@
 // Users Router
 const router = require('express').Router();
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
 const { users } = require('../controllers');
 
 router.post('/', users.create);
 
 // router.use(require('../config/auth'));
 router.get('/', users.index);
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'images');
-  },
-  filename: function(req, file, cb) {   
-      cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-  if(allowedFileTypes.includes(file.mimetype)) {
-      cb(null, true);
-  } else {
-      cb(null, false);
-  }
-}
-
-let upload = multer({ storage, fileFilter });
-
-router.route('/add').post(upload.single('photo'), (req, res) => {
-  const name = req.body.name;
-  const photo = req.file.filename;
-
-  const newUserData = {
-      name,
-      photo
-  }
-
-  const newUser = new User(newUserData);
-
-  newUser.save()
-         .then(() => res.json('User Added'))
-         .catch(err => res.status(400).json('Error: ' + err));
-});
 
 module.exports = router;
