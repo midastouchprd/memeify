79f6faeb45aa5ff5bef094a7b07ecdf6b00e5562
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-09-13 17:07:15 -0700

    find and update post image

--- server/routes/imageposts.js
+++ server/routes/imageposts.js
@@ -1,6 +1,10 @@
+//Image Post router
+//Feel free to change whatever you want, move code into other files and/or completely delete this
+
 const router = require('express').Router();
-const { memePost } = require('../controllers');
+// const { memePost } = require('../controllers');
 const multer = require('multer');
+const { posts } = require('../controllers');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -16,8 +20,29 @@ const upload = multer({storage: storage});
 
 //Request Add New Image
 router.post("/", upload.single("memeImage"), (req, res) => {
-  const newPost = new Posts({
-    author: req.user
+  const newImagePost = new ImagePosts({
+    author: req.body.author,
+    memeImage: req.file.originalname,
+    caption: req.body.caption,
+  });
+  newImagePost
+  .save()
+  .then(() => res.json("New Image Posted!"))
+  .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// //Request Find Image by id
+// router.get("/:id", (req,res) => {
+//   Images.findById(rteq.params.id)
+//   .then((image) => res.json(image))
+//   .catch((err) => res.status(400).json(`Error: ${err}`));
+// });
+
+//Request Find Post Image by ID and Update Post Image
+router.put("/update/:id", upload.single("postName"), (req, res) => {
+  posts.findbyId(req.params.id)
+  .then((post) => {
+    post.author = 
   })
 })
 
